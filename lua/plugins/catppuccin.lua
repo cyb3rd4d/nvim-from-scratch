04e48ee57a5eb5ddb4fdab9928a62324a6111b88
Martial Saunois <msaunois@deezer.com>
2024-01-04 15:23:47 +0100

    Install Lualine and Neogit

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,11 +1,11 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme('catppuccin-mocha')
-    end,
-    lazy = false,
-  },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("catppuccin-mocha")
+		end,
+		lazy = false,
+	},
 }
---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",      -- required
+      "sindrets/diffview.nvim",     -- optional - Diff integration
+      "nvim-telescope/telescope.nvim", -- optional
+    },
+    config = function()
+      local neogit = require("neogit")
+      neogit.setup()
+      vim.keymap.set("n", "<leader>s", function()
+        neogit.open({ kind = "split" })
+      end, {})
+    end,
+  },
+}
