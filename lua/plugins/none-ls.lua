ee654c490411326bcb538601f73d2db4772e9c50
Martial Saunois <msaunois@deezer.com>
2024-01-04 17:58:18 +0100

    Enable formatting on save

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,21 +1,35 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "lewis6991/gitsigns.nvim",
-  },
-  config = function()
-    local null_ls = require('null-ls')
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"lewis6991/gitsigns.nvim",
+	},
+	config = function()
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+		local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.code_actions.gitsigns,
-        null_ls.builtins.code_actions.gomodifytags,
-        null_ls.builtins.code_actions.impl,
-        null_ls.builtins.code_actions.refactoring,
-        null_ls.builtins.diagnostics.luacheck,
-      },
-    })
-  end
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.goimports,
+				null_ls.builtins.code_actions.gitsigns,
+				null_ls.builtins.code_actions.gomodifytags,
+				null_ls.builtins.code_actions.impl,
+				null_ls.builtins.code_actions.refactoring,
+				null_ls.builtins.diagnostics.luacheck,
+			},
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ async = false })
+						end,
+					})
+				end
+			end,
+		})
+	end,
 }
