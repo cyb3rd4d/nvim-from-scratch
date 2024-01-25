af169fb567039d690025b0b0a11bcbcc5068bcd3
Martial Saunois <msaunois@deezer.com>
2024-01-25 16:41:59 +0100

    Add phpactor in the LSP servers

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,60 +1,65 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = true,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		dependencies = {},
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "gopls", "lua_ls", "bashls", "tsserver" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "folke/neodev.nvim", opts = {} },
-		},
-		config = function()
-			require("neodev").setup()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			local lsp = vim.lsp
+  {
+    "williamboman/mason.nvim",
+    config = true,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {},
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "gopls", "lua_ls", "bashls", "tsserver", "phpactor" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "folke/neodev.nvim",   opts = {} },
+    },
+    config = function()
+      require("neodev").setup()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
+      local lsp = vim.lsp
 
-			local handlers = {
-				["textDocument/hover"] = lsp.with(lsp.handlers.hover, { border = "rounded" }),
-				["textDocument/signatureHelp"] = lsp.with(lsp.handlers.signature_help, { border = "rounded" }),
-			}
+      local handlers = {
+        ["textDocument/hover"] = lsp.with(lsp.handlers.hover, { border = "rounded" }),
+        ["textDocument/signatureHelp"] = lsp.with(lsp.handlers.signature_help, { border = "rounded" }),
+      }
 
-			lspconfig.lua_ls.setup({
-				handlers = handlers,
-				capabilities = capabilities,
-				settings = {
-					Lua = {
-						completion = {
-							callSnippet = "Replace",
-						},
-					},
-				},
-			})
+      lspconfig.lua_ls.setup({
+        handlers = handlers,
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            completion = {
+              callSnippet = "Replace",
+            },
+          },
+        },
+      })
 
-			lspconfig.gopls.setup({
-				handlers = handlers,
-				capabilities = capabilities,
-			})
+      lspconfig.gopls.setup({
+        handlers = handlers,
+        capabilities = capabilities,
+      })
 
-			lspconfig.bashls.setup({
-				handlers = handlers,
-				capabilities = capabilities,
-			})
+      lspconfig.bashls.setup({
+        handlers = handlers,
+        capabilities = capabilities,
+      })
 
-			lspconfig.tsserver.setup({
-				handlers = handlers,
-				capabilities = capabilities,
-			})
-		end,
-	},
+      lspconfig.tsserver.setup({
+        handlers = handlers,
+        capabilities = capabilities,
+      })
+
+      lspconfig.phpactor.setup({
+        handlers = handlers,
+        capabilities = capabilities,
+      })
+    end,
+  },
 }
