321bc495e4b359bb8dfeff742a0b050b32c3ac24
Martial Saunois <msaunois@deezer.com>
2024-01-25 16:43:45 +0100

    Add missing fields in treesitter config struct

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,26 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-    configs.setup({
-      ensure_installed = { "lua", "vim", "vimdoc", "query", "javascript", "html", "go", "php", },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+		configs.setup({
+			ensure_installed = { "lua", "vim", "vimdoc", "query", "javascript", "html", "go", "php" },
+			ignore_install = {},
+			modules = {},
+			auto_install = false,
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<leader>t", -- set to `false` to disable one of the mappings
+					node_incremental = "<leader>k",
+					scope_incremental = "<leader>j",
+					node_decremental = "<leader>o",
+				},
+			},
+		})
+	end,
 }
