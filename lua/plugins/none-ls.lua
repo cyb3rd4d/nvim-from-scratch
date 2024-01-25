fa35f8974d66bf2dadd8fa996744485f8ffd1c4d
Martial Saunois <msaunois@deezer.com>
2024-01-25 16:42:47 +0100

    Add phpcsfixer in the formatters

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -11,12 +11,13 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.goimports,
+				null_ls.builtins.formatting.goimports,
 				null_ls.builtins.code_actions.gitsigns,
 				null_ls.builtins.code_actions.gomodifytags,
 				null_ls.builtins.code_actions.impl,
 				null_ls.builtins.code_actions.refactoring,
 				null_ls.builtins.diagnostics.luacheck,
+				null_ls.builtins.formatting.phpcsfixer,
 			},
 			on_attach = function(client, bufnr)
 				if client.supports_method("textDocument/formatting") then
