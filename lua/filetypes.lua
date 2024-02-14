911efb6d4b5062df502db0ca0bc089a05cd579d6
Martial Saunois <msaunois@deezer.com>
2024-02-14 09:33:07 +0100

    Apply starlark syntax to Tiltfiles

--- lua/filetypes.lua
+++ lua/filetypes.lua
@@ -3,5 +3,6 @@ local M = {}
 M.groovy = { "Jenkinsfile", "Jenkinsfile.mep", "Jenkinsfile.rollback", "*.jenkinsfile" }
 M.yaml = { "*.yaml.tmpl", "*.yaml.tpl" }
 M.dockerfile = { "*.dockerfile" }
+M.starlark = { "Tiltfile" }
 
 return M
