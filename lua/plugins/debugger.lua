5b9a171f7e669e21dc7837eac7f26c7e6eee8a33
Martial Saunois <msaunois@deezer.com>
2024-03-27 10:48:53 +0100

    Install missing nvim-nio dependency

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -1,56 +1,57 @@
 return {
-	{
-		"mfussenegger/nvim-dap",
-		dependencies = {
-			"rcarriga/nvim-dap-ui",
-			config = true,
-		},
-		config = function()
-			local dap, dapui = require("dap"), require("dapui")
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "nvim-neotest/nvim-nio",
+      config = true,
+    },
+    config = function()
+      local dap, dapui = require("dap"), require("dapui")
 
-			dap.listeners.before.attach.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.launch.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.event_terminated.dapui_config = function()
-				dapui.close()
-			end
-			dap.listeners.before.event_exited.dapui_config = function()
-				dapui.close()
-			end
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
 
-			dap.adapters.go = function(callback, config)
-				local default_config = {
-					type = "server",
-					host = "127.0.0.1",
-					port = "${port}",
-					executable = {
-						command = "dlv",
-						args = { "dap", "-l", "127.0.0.1:${port}" },
-					},
-					options = {
-						initialize_timeout_sec = 20,
-					},
-				}
+      dap.adapters.go = function(callback, config)
+        local default_config = {
+          type = "server",
+          host = "127.0.0.1",
+          port = "${port}",
+          executable = {
+            command = "dlv",
+            args = { "dap", "-l", "127.0.0.1:${port}" },
+          },
+          options = {
+            initialize_timeout_sec = 20,
+          },
+        }
 
-				if config.mode ~= "remote" then
-					callback(default_config)
-					return
-				end
+        if config.mode ~= "remote" then
+          callback(default_config)
+          return
+        end
 
-				local listener_addr = config.host .. ":" .. config.port
-				default_config.port = config.port
-				default_config.executable.args = { "dap", "-l", listener_addr }
+        local listener_addr = config.host .. ":" .. config.port
+        default_config.port = config.port
+        default_config.executable.args = { "dap", "-l", listener_addr }
 
-				vim.notify("Dynamic remote Go adapter with listener address: " .. listener_addr, vim.log.levels.DEBUG)
+        vim.notify("Dynamic remote Go adapter with listener address: " .. listener_addr, vim.log.levels.DEBUG)
 
-				callback(default_config)
-			end
+        callback(default_config)
+      end
 
-			local vscode = require("dap.ext.vscode")
-			vscode.load_launchjs()
-		end,
-	},
+      local vscode = require("dap.ext.vscode")
+      vscode.load_launchjs()
+    end,
+  },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,65 +1,71 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = true,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		dependencies = {},
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "gopls", "lua_ls", "bashls", "tsserver", "rust_analyzer" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "folke/neodev.nvim", opts = {} },
-		},
-		config = function()
-			require("neodev").setup()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			local lsp = vim.lsp
+  {
+    "williamboman/mason.nvim",
+    config = true,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {},
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "gopls", "lua_ls", "bashls", "tsserver", "rust_analyzer" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "folke/neodev.nvim",   opts = {} },
+    },
+    config = function()
+      require("neodev").setup({
+        library = {
+          plugins = { "nvim-dap-ui" },
+          types = true,
+        },
+      })
 
-			local handlers = {
-				["textDocument/hover"] = lsp.with(lsp.handlers.hover, { border = "rounded" }),
-				["textDocument/signatureHelp"] = lsp.with(lsp.handlers.signature_help, { border = "rounded" }),
-			}
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
+      local lsp = vim.lsp
 
-			lspconfig.lua_ls.setup({
-				handlers = handlers,
-				capabilities = capabilities,
-				settings = {
-					Lua = {
-						completion = {
-							callSnippet = "Replace",
-						},
-					},
-				},
-			})
+      local handlers = {
+        ["textDocument/hover"] = lsp.with(lsp.handlers.hover, { border = "rounded" }),
+        ["textDocument/signatureHelp"] = lsp.with(lsp.handlers.signature_help, { border = "rounded" }),
+      }
 
-			lspconfig.gopls.setup({
-				handlers = handlers,
-				capabilities = capabilities,
-			})
+      lspconfig.lua_ls.setup({
+        handlers = handlers,
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            completion = {
+              callSnippet = "Replace",
+            },
+          },
+        },
+      })
 
-			lspconfig.bashls.setup({
-				handlers = handlers,
-				capabilities = capabilities,
-			})
+      lspconfig.gopls.setup({
+        handlers = handlers,
+        capabilities = capabilities,
+      })
 
-			lspconfig.tsserver.setup({
-				handlers = handlers,
-				capabilities = capabilities,
-			})
+      lspconfig.bashls.setup({
+        handlers = handlers,
+        capabilities = capabilities,
+      })
 
-			lspconfig.rust_analyzer.setup({
-				handlers = handlers,
-				capabilities = capabilities,
-			})
-		end,
-	},
+      lspconfig.tsserver.setup({
+        handlers = handlers,
+        capabilities = capabilities,
+      })
+
+      lspconfig.rust_analyzer.setup({
+        handlers = handlers,
+        capabilities = capabilities,
+      })
+    end,
+  },
 }
