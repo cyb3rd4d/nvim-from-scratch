ace997a3660d0e498707ebfd013e1e4b8590ce6d
Martial Saunois <msaunois@deezer.com>
2024-07-22 14:58:33 +0200

    Upgrade plugins and fix which-key API

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,94 +1,95 @@
 local wk = require("which-key")
 
-wk.register({
-	["<C-h>"] = { "<cmd>TmuxNavigateLeft<cr>", "Go to the left window" },
-	["<C-l>"] = { "<cmd>TmuxNavigateRight<cr>", "Go to the right window" },
-	["<C-j>"] = { "<cmd>TmuxNavigateUp<cr>", "Go to the top window" },
-	["<C-k>"] = { "<cmd>TmuxNavigateDown<cr>", "Go to the bottom window" },
-	["<C-x>"] = { "<cmd>bdelete<cr>", "Close the current buffer" },
-}, {
-	mode = "n",
+wk.add({
+	{ "<C-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "Go to the left window" },
+	{ "<C-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Go to the right window" },
+	{ "<C-j>", "<cmd>TmuxNavigateUp<cr>", desc = "Go to the top window" },
+	{ "<C-k>", "<cmd>TmuxNavigateDown<cr>", desc = "Go to the bottom window" },
+	{ "<C-x>", "<cmd>bdelete<cr>", desc = "Close the current buffer" },
 })
 
-wk.register({
-	["<C-h>"] = { "<Left>", "Move the cursor on the left" },
-	["<C-l>"] = { "<Right>", "Move the cursor on the right" },
-	["<C-k>"] = { "<Up>", "Move the cursor above" },
-	["<C-j>"] = { "<Down>", "Move the cursor below" },
-}, {
-	mode = "i",
+wk.add({
+	{
+		mode = "i",
+		{ "<C-h>", "<Left>", desc = "Move the cursor on the left" },
+		{ "<C-l>", "<Right>", desc = "Move the cursor on the right" },
+		{ "<C-k>", "<Up>", desc = "Move the cursor above" },
+		{ "<C-j>", "<Down>", desc = "Move the cursor below" },
+	},
 })
 
 local telescope_builtin = require("telescope.builtin")
 local dap = require("dap")
 
-wk.register({
+wk.add({
 	-- Find things
-	f = {
-		name = "Find",
-		-- File pickers
-		f = { telescope_builtin.find_files, "Find file" },
-		a = { telescope_builtin.live_grep, "Live grep" },
-		w = { telescope_builtin.grep_string, "Search the word under the cursor" },
-		cb = { telescope_builtin.current_buffer_fuzzy_find, "Search in the current buffer" },
+	{ "<leader>f", group = "Find" },
+	-- File pickers
+	{ "<leader>ff", telescope_builtin.find_files, desc = "Find file" },
+	{ "<leader>fa", telescope_builtin.live_grep, desc = "Live grep" },
+	{ "<leader>fw", telescope_builtin.grep_string, desc = "Search the word under the cursor" },
+	{ "<leader>fcb", telescope_builtin.current_buffer_fuzzy_find, desc = "Search in the current buffer" },
 
-		-- LSP pickers
-		ld = { telescope_builtin.lsp_definitions, "Find LSP definitions" },
+	-- LSP pickers
+	{ "<leader>fld", telescope_builtin.lsp_definitions, desc = "Find LSP definitions" },
 
-		-- Vim pickers
-		b = { telescope_builtin.buffers, "Find buffer" },
-		h = { telescope_builtin.help_tags, "Find help tags" },
-		m = { telescope_builtin.marks, "Find marks" },
-		q = { telescope_builtin.quickfix, "Find quickfix" },
-		ll = { telescope_builtin.loclist, "Find location" },
-		jl = { telescope_builtin.jumplist, "Find jump" },
-		k = { telescope_builtin.keymaps, "Find keymap" },
+	-- Vim pickers
+	{ "<leader>fb", telescope_builtin.buffers, desc = "Find buffer" },
+	{ "<leader>fh", telescope_builtin.help_tags, desc = "Find help tags" },
+	{ "<leader>fm", telescope_builtin.marks, desc = "Find marks" },
+	{ "<leader>fq", telescope_builtin.quickfix, desc = "Find quickfix" },
+	{ "<leader>fll", telescope_builtin.loclist, desc = "Find location" },
+	{ "<leader>fjl", telescope_builtin.jumplist, desc = "Find jump" },
+	{ "<leader>fk", telescope_builtin.keymaps, desc = "Find keymap" },
 
-		-- Git pickers
-		gc = { telescope_builtin.git_commits, "Find commits" },
-		gbc = { telescope_builtin.git_bcommits, "Find commits for current buffer" },
-		gbr = { telescope_builtin.git_branches, "Find git branch" },
-		gs = { telescope_builtin.git_status, "Find git status" },
-		ga = { telescope_builtin.git_stash, "Find git stash" },
+	-- Git pickers
+	{ "<leader>fgc", telescope_builtin.git_commits, desc = "Find commits" },
+	{ "<leader>fgbc", telescope_builtin.git_bcommits, desc = "Find commits for current buffer" },
+	{ "<leader>fgbr", telescope_builtin.git_branches, desc = "Find git branch" },
+	{ "<leader>fgs", telescope_builtin.git_status, desc = "Find git status" },
+	{ "<leader>fga", telescope_builtin.git_stash, desc = "Find git stash" },
 
-		-- Treesitter
-		tree = { telescope_builtin.treesitter, "Find Treesitter object" },
-	},
+	-- Treesitter
+	{ "<leader>ftree", telescope_builtin.treesitter, desc = "Find Treesitter object" },
 	-- Debugger
-	db = { dap.toggle_breakpoint, "Toogle debug breakpoint" },
-}, { prefix = "<leader>" })
+	{ "<leader>db", dap.toggle_breakpoint, desc = "Toogle debug breakpoint" },
+})
 
 local dap_ui_widgets = require("dap.ui.widgets")
 local dapui = require("dapui")
--- local dap_go = require("dap-go")
+local dap_go = require("dap-go")
 
 -- Debugger
-wk.register({
-	["<F5>"] = { dap.continue, "Start debugging" },
-	["<F6>"] = { dap.terminate, "Stop debugging" },
-	["<F7>"] = { dapui.close, "Stop debugging" },
-	["<F9>"] = { dap.step_back, "Step back" },
-	["<F10>"] = { dap.step_over, "Step over" },
-	["<F11>"] = { dap.step_into, "Step into" },
-	["<F12>"] = { dap.step_out, "Step out" },
-	-- ["<leader>dt"] = { dap_go.debug_test, "Debug Go test" },
-	-- ["<leader>dlt"] = { dap_go.debug_last_test, "Debug last Go test" },
-	["<leader>dh"] = { dap_ui_widgets.hover, "Evaluate in a floating window" },
-	["<leader>dp"] = { dap_ui_widgets.preview, "Evaluate in a buffer" },
-	["<leader>df"] = {
-		function()
-			dap_ui_widgets.centered_float(dap_ui_widgets.frames)
-		end,
-		"View the current frames in a centered floating window",
+wk.add({
+	{ "<F5>", dap.continue, desc = "Start debugging" },
+	{ "<F6>", dap.terminate, desc = "Stop debugging" },
+	{ "<F7>", dapui.close, desc = "Stop debugging" },
+	{ "<F9>", dap.step_back, desc = "Step back" },
+	{ "<F10>", dap.step_over, desc = "Step over" },
+	{ "<F11>", dap.step_into, desc = "Step into" },
+	{ "<F12>", dap.step_out, desc = "Step out" },
+	{
+		mode = { "n", "i" },
+		{ "<leader>d", group = "Debugger" },
+		{ "<leader>dt", dap_go.debug_test, desc = "Debug Go test" },
+		{ "<leader>dlt", dap_go.debug_last_test, desc = "Debug last Go test" },
+		{ "<leader>dh", dap_ui_widgets.hover, desc = "Evaluate in a floating window" },
+		{ "<leader>dp", dap_ui_widgets.preview, desc = "Evaluate in a buffer" },
+		{
+			"<leader>df",
+			function()
+				dap_ui_widgets.centered_float(dap_ui_widgets.frames)
+			end,
+			desc = "View the current frames in a centered floating window",
+		},
+		{
+			"<leader>ds",
+			function()
+				dap_ui_widgets.centered_float(dap_ui_widgets.scopes)
+			end,
+			desc = "View the current scopes in a centered floating window",
+		},
 	},
-	["<leader>ds"] = {
-		function()
-			dap_ui_widgets.centered_float(dap_ui_widgets.scopes)
-		end,
-		"View the current scopes in a centered floating window",
-	},
-}, {
-	mode = { "n", "v" },
 })
 
 -- Use LspAttach autocommand to only map the following keys
@@ -102,113 +103,141 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		local opts = { buffer = ev.buf }
 		local lsp = vim.lsp
 
-		wk.register({
-			gD = { lsp.buf.declaration, "Go to declaration" },
-			gd = { lsp.buf.definition, "Go to definition" },
-			K = { lsp.buf.hover, "Show documentation" },
-			gi = { telescope_builtin.lsp_implementations, "Find implementations" },
-			["<leader>k"] = { lsp.buf.signature_help, "Show signature" },
-			["<leader>wa"] = { lsp.buf.add_workspace_folder, "Add folder to workspace" },
-			["<leader>wr"] = { lsp.buf.remove_workspace_folder, "Remove folder to workspace" },
-			["<leader>wl"] = {
-				function()
-					print(vim.inspect(lsp.buf.list_workspace_folders()))
-				end,
-				"List folders in workspace",
+		wk.add({
+			{
+				buffer = opts.buffer,
+				{ "gD", lsp.buf.declaration, desc = "Go to declaration" },
+				{ "gd", lsp.buf.definition, desc = "Go to definition" },
+				{ "K", lsp.buf.hover, desc = "Show documentation" },
+				{ "gi", telescope_builtin.lsp_implementations, desc = "Find implementations" },
+				{ "<leader>k", lsp.buf.signature_help, desc = "Show signature" },
+				{ "<leader>wa", lsp.buf.add_workspace_folder, desc = "Add folder to workspace" },
+				{ "<leader>wr", lsp.buf.remove_workspace_folder, desc = "Remove folder to workspace" },
+				{
+					"<leader>wl",
+					function()
+						print(vim.inspect(lsp.buf.list_workspace_folders()))
+					end,
+					desc = "List folders in workspace",
+				},
+				{ "<leader>D", lsp.buf.type_definition, desc = "Type definition" },
+				{ "<leader>rn", lsp.buf.rename, desc = "Rename symbol" },
+				{
+					"gr",
+					telescope_builtin.lsp_references,
+					desc = "List all references of the symbol under the cursor",
+				},
+				{
+					"<leader>cf",
+					function()
+						lsp.buf.format({ async = false })
+					end,
+					desc = "Format code",
+				},
 			},
-			["<leader>D"] = { lsp.buf.type_definition, "Type definition" },
-			["<leader>rn"] = { lsp.buf.rename, "Rename symbol" },
-			gr = { telescope_builtin.lsp_references, "List all references of the symbol under the cursor" },
-			["<leader>cf"] = {
-				function()
-					lsp.buf.format({ async = false })
-				end,
-				"Format code",
-			},
-		}, { opts })
+		})
 
-		wk.register({
-			["<leader>ca"] = { lsp.buf.code_action, "Code actions" },
-		}, {
-			mode = { "n", "v" },
-			buffer = opts.buffer,
+		wk.add({
+			{ "<leader>ca", lsp.buf.code_action, desc = "Code actions", mode = { "n", "v" }, buffer = opts.buffer },
 		})
 	end,
 })
 
 -- Git
-wk.register({
-	["<leader>gs"] = { "<cmd>LazyGit<cr>", "Toggle Lazygit" },
-	["<leader>gg"] = { "<cmd>G<cr>", "Toggle Fugitive" },
-}, {
-	mode = "n",
+wk.add({
+	{ "<leader>g", group = "Git" },
+	{ "<leader>gs", "<cmd>LazyGit<cr>", desc = "Toggle Lazygit" },
+	{ "<leader>gg", "<cmd>G<cr>", desc = "Toggle Fugitive" },
 })
 
 -- File tree
-wk.register({
-	["<C-n>"] = { "<cmd>Neotree toggle reveal_force_cwd<cr>", "Open Neotree" },
+wk.add({
+	{ "<C-n>", "<cmd>Neotree toggle reveal_force_cwd<cr>", desc = "Open Neotree" },
 })
 
 -- Terminal
-wk.register({
-	["<A-v>"] = { "<cmd>ToggleTerm direction=vertical size=80<cr>", "Toggle vertical terminal" },
-	["<A-h>"] = { "<cmd>ToggleTerm direction=horizontal<cr>", "Toggle horizontal terminal" },
-	["<A-f>"] = { "<cmd>ToggleTerm direction=float<cr>", "Toggle floating terminal" },
-}, {
-	mode = "n",
+wk.add({
+	{ "<A-v>", "<cmd>ToggleTerm direction=vertical size=120<cr>", desc = "Toggle vertical terminal" },
+	{ "<A-h>", "<cmd>ToggleTerm direction=horizontal size=20<cr>", desc = "Toggle horizontal terminal" },
+	{ "<A-f>", "<cmd>ToggleTerm direction=float<cr>", desc = "Toggle floating terminal" },
 })
 
-wk.register({
-	["<A-v>"] = { "<C-\\><C-n><cmd>ToggleTerm direction=vertical<cr>", "Toggle vertical terminal" },
-	["<A-h>"] = { "<C-\\><C-n><cmd>ToggleTerm direction=horizontal<cr>", "Toggle horizontal terminal" },
-	["<A-f>"] = { "<C-\\><C-n><cmd>ToggleTerm direction=float<cr>", "Toggle floating terminal" },
-	["<C-h>"] = { "<C-\\><C-n><C-w>h", "While in terminal mode, move to the left window" },
-	["<C-l>"] = { "<C-\\><C-n><C-w>l", "While in terminal mode, move to the right window" },
-	["<C-j>"] = { "<C-\\><C-n><C-w>j", "While in terminal mode, move to the top window" },
-	["<C-k>"] = { "<C-\\><C-n><C-w>k", "While in terminal mode, move to the bottom window" },
-}, {
-	mode = "t",
+wk.add({
+	{
+		mode = "t",
+		{ "<A-v>", "<C-\\><C-n><cmd>ToggleTerm direction=vertical<cr>", desc = "Toggle vertical terminal" },
+		{ "<A-h>", "<C-\\><C-n><cmd>ToggleTerm direction=horizontal<cr>", desc = "Toggle horizontal terminal" },
+		{ "<A-f>", "<C-\\><C-n><cmd>ToggleTerm direction=float<cr>", desc = "Toggle floating terminal" },
+		{
+			"<C-h>",
+			"<C-\\><C-n><C-w>h",
+			desc = "While in terminal mode, move to the left window",
+		},
+		{
+			"<C-l>",
+			"<C-\\><C-n><C-w>l",
+			desc = "While in terminal mode, move to the right window",
+		},
+		{
+			"<C-j>",
+			"<C-\\><C-n><C-w>j",
+			desc = "While in terminal mode, move to the top window",
+		},
+		{
+			"<C-k>",
+			"<C-\\><C-n><C-w>k",
+			desc = "While in terminal mode, move to the bottom window",
+		},
+	},
 })
 
 local trouble = require("trouble")
 
-wk.register({
-	["<leader>xx"] = {
+wk.add({
+	{
+		"<leader>xx",
 		function()
 			trouble.toggle()
 		end,
-		"Toggle Trouble",
+		desc = "Toggle Trouble",
 	},
-	["<leader>xw"] = {
+	{
+		"<leader>xw",
 		function()
 			trouble.toggle("workspace_diagnostics")
 		end,
-		"Toggle Trouble workspace diagnostics",
+		desc = "Toggle Trouble workspace diagnostics",
 	},
-	["<leader>xd"] = {
+	{
+		"<leader>xd",
 		function()
 			trouble.toggle("document_diagnostics")
 		end,
-		"Toggle Trouble document diagnostics",
+		desc = "Toggle Trouble document diagnostics",
 	},
-	["<leader>xq"] = {
+	{
+		"<leader>xq",
 		function()
 			trouble.toggle("quickfix")
 		end,
-		"Toggle Trouble quickfix",
+		desc = "Toggle Trouble quickfix",
 	},
-	["<leader>xl"] = {
+	{
+		"<leader>xl",
 		function()
 			trouble.toggle("loclist")
 		end,
-		"Toggle Trouble location list",
+		desc = "Toggle Trouble location list",
 	},
-	["gR"] = {
+	{
+		"gR",
 		function()
 			trouble.toggle("lsp_references")
 		end,
-		"Toggle Trouble LSP references",
+		desc = "Toggle Trouble LSP references",
 	},
-}, {
-	mode = "n",
+})
+
+wk.add({
+	{ "-", "<cmd>Oil<cr>", desc = "Open Oil in the parent directory" },
 })
