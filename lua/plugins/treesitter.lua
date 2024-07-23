4fa94bdc52626103f67494814aac3a7c2c0890d3
Martial Saunois <msaunois@deezer.com>
2024-07-23 14:24:31 +0200

    Add PHP, JSON, GraphQL and HTTP treesitter parsers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,26 +1,40 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
 
-		configs.setup({
-			ensure_installed = { "lua", "vim", "vimdoc", "query", "javascript", "html", "go", "php", "sql" },
-			ignore_install = {},
-			modules = {},
-			auto_install = false,
-			sync_install = false,
-			highlight = { enable = true },
-			indent = { enable = true },
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<leader>t", -- set to `false` to disable one of the mappings
-					node_incremental = "<leader>k",
-					scope_incremental = "<leader>j",
-					node_decremental = "<leader>o",
-				},
-			},
-		})
-	end,
+    configs.setup({
+      ensure_installed = {
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "javascript",
+        "html",
+        "go",
+        "php",
+        "sql",
+        "xml",
+        "json",
+        "graphql",
+        "http",
+      },
+      ignore_install = {},
+      modules = {},
+      auto_install = false,
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<leader>t", -- set to `false` to disable one of the mappings
+          node_incremental = "<leader>k",
+          scope_incremental = "<leader>j",
+          node_decremental = "<leader>o",
+        },
+      },
+    })
+  end,
 }
