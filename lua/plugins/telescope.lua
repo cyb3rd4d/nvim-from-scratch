faaabb8df9fc99e7ee71654e72d8a32558822bfd
Martial Saunois <msaunois@deezer.com>
2024-07-23 14:23:33 +0200

    Remove Telescope config for trouble

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,30 +1,20 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		branch = "0.1.x",
-		dependencies = {
-			{ "nvim-lua/plenary.nvim" },
-			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-			{ "nvim-telescope/telescope-ui-select.nvim", config = true },
-			{ "nvim-telescope/telescope-dap.nvim" },
-			{ "rcarriga/nvim-notify" },
-		},
-		config = function()
-			local telescope = require("telescope")
-			--    local trouble = require("trouble.providers.telescope")
-			--
-			-- telescope.setup({
-			--      defaults = {
-			--        mappings = {
-			--          i = { ["<C-t>"] = trouble.open_with_trouble },
-			--          n = { ["<C-t>"] = trouble.open_with_trouble },
-			--        },
-			--      },
-			--    })
-			telescope.setup({})
-			telescope.load_extension("ui-select")
-			telescope.load_extension("notify")
-			telescope.load_extension("dap")
-		end,
-	},
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+      { "nvim-telescope/telescope-ui-select.nvim",  config = true },
+      { "nvim-telescope/telescope-dap.nvim" },
+      { "rcarriga/nvim-notify" },
+    },
+    config = function()
+      local telescope = require("telescope")
+      telescope.setup({})
+      telescope.load_extension("ui-select")
+      telescope.load_extension("notify")
+      telescope.load_extension("dap")
+    end,
+  },
 }
