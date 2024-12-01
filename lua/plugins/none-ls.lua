aff37863f9da7cc04b437f103733762ed2f15b17
Martial Saunois <msaunois@deezer.com>
2024-12-01 17:01:49 +0100

    Uninstall stylua

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -10,7 +10,6 @@ return {
 
     null_ls.setup({
       sources = {
-        null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.goimports,
         null_ls.builtins.formatting.terraform_fmt,
         null_ls.builtins.code_actions.gitsigns,
